4334a157cfed4a178c98c2bb0a39c9336641de94
jin <grccorps@gmail.com>
2021-09-23 15:46:33 +0900

    sepolicy: add a note to set Lineage OS parts before including a21s-sepolicy

--- a21s-sepolicy.mk
+++ a21s-sepolicy.mk
@@ -9,6 +9,9 @@ BOARD_SEPOLICY_DIRS += device/samsung/a21s-sepolicy/vendor
 BOARD_SEPOLICY_DIRS += device/samsung/a21s-sepolicy/tracking_denials
 
 # LineageOS
+# Important: if you are using this sepolicy for building Lineage OS, please set BOARD_SEPOLICY_LINEAGE_OS to true
+# BEFORE including a21s-sepolicy, as setting this value after inclusion of this makefile will result in
+# Lineage OS parts being left out, resulting in build failure or other problems.
 ifeq ($(BOARD_SEPOLICY_LINEAGE_OS),true)
 BOARD_SEPOLICY_DIRS += device/samsung/a21s-sepolicy/lineage
 endif
