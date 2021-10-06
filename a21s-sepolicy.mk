edc836a946788a10e1d75036d1b985913abe3334
jin <grccorps@gmail.com>
2021-10-06 22:25:30 +0900

    sepolicy: add option for building GSI
    these policies are for building a ROM with that have all the HALs from stock

--- a21s-sepolicy.mk
+++ a21s-sepolicy.mk
@@ -8,10 +8,14 @@ BOARD_SEPOLICY_DIRS += device/samsung/a21s-sepolicy/vendor
 # Dontaudits
 BOARD_SEPOLICY_DIRS += device/samsung/a21s-sepolicy/tracking_denials
 
-# LineageOS
-# Important: if you are using this sepolicy for building Lineage OS, please set BOARD_SEPOLICY_LINEAGE_OS to true
+# LineageOS / GSI
+# Important: if you are using this sepolicy for building Lineage OS or GSI, please set the appropriate flag to true
 # BEFORE including a21s-sepolicy, as setting this value after inclusion of this makefile will result in
 # Lineage OS parts being left out, resulting in build failure or other problems.
 ifeq ($(BOARD_SEPOLICY_LINEAGE_OS),true)
 BOARD_SEPOLICY_DIRS += device/samsung/a21s-sepolicy/lineage
+endif
+
+ifeq ($(BOARD_SEPOLICY_SEC_GSI),true)
+BOARD_SEPOLICY_DIRS += device/samsung/a21s-sepolicy/vendor_gsi
 endif
